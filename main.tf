68c193bc3268b6579e7a90ceb4a4aae04e82f98f
Lawrence VanOverbeke <lawrencevanoverbeke@MW-LVANOVERBEKE.attlocal.net>
2020-08-28 13:33:53 -0700

    edited =

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   network_profile {
     load_balancer_sku = "Standard"
-    load_balancer_profile {
+    load_balancer_profile = {
       outbound_ports_allocated = "4000"
       managed_outbound_ip_count = "4"
     }
