2088da65e3629e4924d4b39b9f4f07b9073802b4
Sudhashri Joshi <sudhashri.joshi@mavenwave.com>
2020-08-31 09:25:13 -0500

    referring the AKS module

--- main.tf
+++ main.tf
@@ -1,51 +1,8 @@
-resource "azurerm_kubernetes_cluster" "k8s" {
-  name                    = var.cluster_name
-  location                = var.location
-  resource_group_name     = var.resource_group_name
-  dns_prefix              = var.dns_prefix
-  private_cluster_enabled = true
-  /*
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
-  }
-*/
-  identity {
-    type = "SystemAssigned"
-  }
-
-  /*   azure_active_directory {
-    managed                = true
-    admin_group_object_ids = var.aks_config.admin_group
-  } */
-
-  default_node_pool {
-    name           = "agentpool"
-    node_count     = var.agent_count
-    vm_size        = "Standard_D2_v2"
-    vnet_subnet_id = "/subscriptions/63a4467b-b46e-4f35-b623-1e5b076ef28c/resourceGroups/rg-internalnetwork-dev-001/providers/Microsoft.Network/virtualNetworks/vnet-dev-internal-app-centralus-001/subnets/snet-dev-build-centralus-001"
-  }
-
-  addon_profile {
-    oms_agent {
-      enabled = false
-    }
-  }
-
-  network_profile {
-    load_balancer_sku  = "Standard"
-    network_plugin     = "azure"
-    service_cidr       = "10.1.0.0/18"
-    docker_bridge_cidr = "172.17.0.1/16"
-    dns_service_ip     = "10.1.0.10"
-  }
-
-  tags = {
-    Environment = "Development"
-  }
+module "jenkins_k8cluster" {
+  //source = "git@bitbucket.org:mavenwave/trg-terraform-build-aks"
+  source = "../trg-terraform-build-aks"
+  aks_info = var.aks_info
+  resource_group = data.terraform_remote_state.resource_group.outputs.id.value[2]
 }
 
 /*******************
--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,13 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
+data "terraform_remote_state" "resource_group" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-dev-001"
+    storage_account_name = "stdevterraformstate002"
+    container_name       = "bootstrapstate"
+    key                  = "terraform.tfstate"
+  }
+}
