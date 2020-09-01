00b8d53857cf61004ce8552f4e777bd4cc28cc90
Lawrence VanOverbeke <lawrencevanoverbeke@MW-LVANOVERBEKE.attlocal.net>
2020-09-01 13:34:15 -0700

    added helm chart

--- main.tf
+++ main.tf
@@ -47,6 +47,21 @@ resource "azurerm_private_dns_zone_virtual_network_link" "link_bastion_cluster" 
   virtual_network_id = "/subscriptions/63a4467b-b46e-4f35-b623-1e5b076ef28c/resourceGroups/rg-internalnetwork-dev-001/providers/Microsoft.Network/virtualNetworks/vnet-dev-internal-mgmt-centralus-001"
 }
 
+resource "helm_release" "trg_ha_proxy" {
+  name    = "build-ha-proxy"
+  reposity = "https://haproxytech.github.io/helm-charts"
+  chart    = "kubernetes-ingress"
+  version = "1.4.4"
+  set {
+    name = "controller.kind"
+    value = "DaemonSet"
+  }
+  set {
+    name = "controller.ingressClass"
+    value = "haproxy"
+  }
+}
+
 resource "kubernetes_ingress" "k8_ingress" {
   metadata {
     name = "k8-ingress"
