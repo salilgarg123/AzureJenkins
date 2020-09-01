b6efe7ce2912baf029da2389bc857a05c0bd5115
Lawrence VanOverbeke <lawrencevanoverbeke@MW-LVANOVERBEKE.attlocal.net>
2020-09-01 13:42:22 -0700

    Revert "added helm provider"
    This reverts commit 142020fb26e8fff9ab376cd26a49388366531bd4.

--- provider.tf
+++ provider.tf
@@ -9,7 +9,3 @@ provider "kubernetes" {
   client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
 }
-
-provider "helm" {
-  version = "1.2.4"
-}
