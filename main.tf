70cd1ac938986b84395fe127444185d9978c9a95
Sudhashri Joshi <sudhashri.joshi@mavenwave.com>
2020-09-01 12:59:04 -0500

    format

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   resource_group_name     = var.resource_group_name
   dns_prefix              = var.dns_prefix
   private_cluster_enabled = true
-  
+
 
   service_principal {
     client_id     = "dd7a40f7-6ddd-443c-bb7f-2a2b3455891c"
@@ -39,12 +39,12 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
 # Link the Bastion Vnet to the Private DNS Zone generated to resolve the Server IP from the URL in Kubeconfig
 resource "azurerm_private_dns_zone_virtual_network_link" "link_bastion_cluster" {
-  name = "dnslink-bastion-cluster"
+  name                  = "dnslink-bastion-cluster"
   private_dns_zone_name = join(".", slice(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn), 1, length(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn))))
   resource_group_name   = "MC_rg-aks-dev-001_${var.cluster_name}_centralus"
   //resource_group_name   = var.resource_group_name //"MC_${var.resource_group_name}_${azurerm_kubernetes_cluster.k8s.name}_${var.location}"
   //virtual_network_id    = azurerm_virtual_network.vnet_bastion.id
-  virtual_network_id    = "/subscriptions/63a4467b-b46e-4f35-b623-1e5b076ef28c/resourceGroups/rg-internalnetwork-dev-001/providers/Microsoft.Network/virtualNetworks/vnet-dev-internal-mgmt-centralus-001"
+  virtual_network_id = "/subscriptions/63a4467b-b46e-4f35-b623-1e5b076ef28c/resourceGroups/rg-internalnetwork-dev-001/providers/Microsoft.Network/virtualNetworks/vnet-dev-internal-mgmt-centralus-001"
 }
 
 resource "kubernetes_ingress" "k8_ingress" {
@@ -137,6 +137,6 @@ resource "kubernetes_service" "jenkins_service" {
       port        = 8080
       target_port = 8080
     }
-    type       = "LoadBalancer"
+    type = "LoadBalancer"
   }
-} 
+}
