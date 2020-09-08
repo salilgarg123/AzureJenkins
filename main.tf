9785dd6d2b03355ad2b13662e50784a1c4c8e89e
Sudhashri Joshi <sudhashri.joshi@mavenwave.com>
2020-09-08 10:33:59 -0500

    code cleanup

--- main.tf
+++ main.tf
@@ -40,36 +40,14 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
-// add to cluster module
 # Link the Bastion Vnet to the Private DNS Zone generated to resolve the Server IP from the URL in Kubeconfig
 resource "azurerm_private_dns_zone_virtual_network_link" "link_bastion_cluster" {
   name                  = "dnslink-bastion-cluster"
   private_dns_zone_name = join(".", slice(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn), 1, length(split(".", azurerm_kubernetes_cluster.k8s.private_fqdn))))
   resource_group_name   = "MC_rg-aks-dev-001_${var.cluster_name}_centralus"
-  //resource_group_name   = var.resource_group_name //"MC_${var.resource_group_name}_${azurerm_kubernetes_cluster.k8s.name}_${var.location}"
-  //virtual_network_id    = azurerm_virtual_network.vnet_bastion.id
   virtual_network_id = "/subscriptions/63a4467b-b46e-4f35-b623-1e5b076ef28c/resourceGroups/rg-internalnetwork-dev-001/providers/Microsoft.Network/virtualNetworks/vnet-dev-internal-mgmt-centralus-001"
 }
-/*
-resource "helm_release" "trg_ha_proxy" {
-  name    = "build-ha-proxy"
-  repository = "https://haproxytech.github.io/helm-charts"
-  chart    = "kubernetes-ingress"
-  version = "1.4.4"
-  set {
-    name = "controller.kind"
-    value = "DaemonSet"
-  }
-  set {
-    name = "controller.ingressClass"
-    value = "haproxy"
-  }
-  set {
-    name = "controller.daemonset.userHostPort"
-    value = true
-  }
-}
-*/
+
 resource "helm_release" "trg_ingress" {
   name = "nginx-ingress"
   repository = "https://kubernetes-charts.storage.googleapis.com/"
@@ -86,6 +64,12 @@ resource "helm_release" "trg_jenkins" {
   repository = "https://charts.jenkins.io"
   chart    = "jenkins"
   version = "2.6.1"
+/*   values = [<<EOF
+    persistence:
+      enabled: true
+      size: "4Gi"
+    EOF
+    ] */
   set {
     name = "master.ingress.enabled"
     value = true
