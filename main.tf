7e2366b87135006c08b2fd8c7c48761830406dac
Sudhashri Joshi <sudhashri.joshi@mavenwave.com>
2020-09-16 10:14:37 -0500

    removed duplicated helm_release resource

--- main.tf
+++ main.tf
@@ -5,30 +5,6 @@ module "jenkins_k8cluster" {
   management_vnet_id = var.management_vnet_id
 }
 
-resource "helm_release" "trg_jenkins" {
-  name    = "build-jenkins"
-  repository = "https://charts.jenkins.io"
-  chart    = "jenkins"
-  version = "2.6.1"
-  
-  set {
-    name = "master.ingress.enabled"
-    value = true
-  }
-  set {
-    name = "master.ingress.path"
-    value = "/"
-  }
-  set {
-    name = "master.ingress.annotations.kubernetes\\.io/ingress\\.class"
-    value = "nginx"
-  }
-  set {
-    name = "master.ingress.apiVersion"
-    value = "networking.k8s.io/v1beta1"
-  }
-}
-
 resource "azurerm_managed_disk" "jenkins_managed_disk" {
     lifecycle {
           prevent_destroy = true
