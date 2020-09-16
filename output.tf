240d7d65cc44caa902bf575516e3f9fa8c5490a9
Sudhashri Joshi <sudhashri.joshi@mavenwave.com>
2020-09-16 10:49:29 -0500

    Updated outputs

--- output.tf
+++ output.tf
@@ -1,26 +1,30 @@
-/* output "client_key" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+output "client_key" {
+  value = module.jenkins_k8cluster.kube_config.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+  value = module.jenkins_k8cluster.kube_config.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  value = module.jenkins_k8cluster.kube_config.cluster_ca_certificate
+  sensitive = true
 }
 
 output "cluster_username" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+  value = module.jenkins_k8cluster.kube_config.username
 }
 
 output "cluster_password" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+  value = module.jenkins_k8cluster.kube_config.password
+  sensitive = true
 }
- */
 
 output "kube_config" {
   value = module.jenkins_k8cluster.kube_config_raw
+  sensitive = true
 }
 
 output "host" {
