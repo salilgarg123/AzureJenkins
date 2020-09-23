7e42365e15d22e695df48f5e8f46f3ebcdb50eb4
Salil Garg <salil.garg@mavenwave.com>
2020-09-23 18:31:23 +0530

    Code reverted to security tab

--- main.tf
+++ main.tf
@@ -109,9 +109,5 @@ resource "helm_release" "trg_jenkins" {
     value = "EXCLUSIVE"
   }
 
-  set {
-    name  = "master.useSecurity"
-    value = "false"
-  }
-  depends_on = [module.jenkins_k8cluster]
+   depends_on = [module.jenkins_k8cluster]
 }
